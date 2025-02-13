9ba3b88149e801e5e65b31ba95e005a4db7cd46d
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-02-13 19:19:30 +0530

    feat: add logout button on header component
    Added logout button on the header component and added props to hide it

--- greendzine-assignment/src/components/Header.jsx
+++ greendzine-assignment/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styles/Header.css';
 
-const Header = () => {
+const Header = ({isLogoutButtonHidden = true}) => {
   return (
-    <header className="header">
+    <header className={"header header-container"}>
       <h1>Analytics Dashboard</h1>
+      <button className={`logout-btn ${isLogoutButtonHidden? "hidden":""}`}>Logout</button>
     </header>
   );
 };
