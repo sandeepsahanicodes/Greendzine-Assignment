4f6858d6ac6e7cc3ff1a880f72e72ba1f0fac604
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-02-14 21:44:23 +0530

    refactor: remove redundant code

--- greendzine-assignment/src/pages/ResendOtp.jsx
+++ greendzine-assignment/src/pages/ResendOtp.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'
-import generateOTP from '../utilities/otpGenerator';
 import Header from '../components/Header';
 import CardView from '../components/CardView';
 import Footer from '../components/Footer';
 
-
 const ResendOtp = () => {
-    const navigate = useNavigate()
-
-    const handleResendOTP = () => {
-        const otp = generateOTP();
-        alert(`Your new OTP is: ${otp}`);
-        localStorage.setItem('otp', otp);
-        localStorage.setItem('otpExpiry', Date.now() + 30000);
-        navigate('/verify-otp');
-      };
-
     return (
         <div style={{display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100vh"}}>
         <Header />
         <div style={{ display: "flex", justifyContent: "center" }}>
-            <CardView heading="Enter Otp sent to Email" placeholder="OTP" btnText="Validate" isResentOtpContainerHidden={false} onButtonTap={handleResendOTP} />
+            {/* Here not sending the otp generation function as it is availabe on the CardView itself */}
+            <CardView heading="Enter Otp sent to Email" placeholder="OTP" btnText="Validate" isResentOtpContainerHidden={false} />
         </div>
         <Footer />
         </div>
